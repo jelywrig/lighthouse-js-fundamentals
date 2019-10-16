f52bc1b100e66865f1736b6d9a0e9ca35177464e
Jessie Wright <jessie.wright@gmail.com>
2019-10-16 20:46:15 +0000

    completed lastIndexOf exercise

---
+++ lastIndexOf.js
@@ -0,0 +1,14 @@
+const lastIndexOf = function (array, num){
+  let result = -1;
+  array.forEach(function (item, index){
+    if (item === num ) result = index;
+    return item;
+  });
+  return result;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
