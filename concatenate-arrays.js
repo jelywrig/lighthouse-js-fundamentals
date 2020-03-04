f5b2bf3c6de7b1dde1f2de6ecd0b217f006052df
Jessie Wright <jessie.wright@gmail.com>
2020-03-04 23:24:12 +0000

    completed exercises

---
+++ concatenate-arrays.js
@@ -0,0 +1,12 @@
+function concat(arry1, arry2){
+  let result = [];
+  for (const item of arry1) {
+    result.push(item);
+  }
+  for (const item of arry2) {
+    result.push(item);
+  }
+  return result;
+}
+
+
